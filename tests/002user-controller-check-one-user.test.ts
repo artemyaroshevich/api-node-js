ea757f537889efc3c8d003a245bf44b28e7901af
artemyaroshevich <artem.yaroshevich.2205@gmail.com>
2024-11-10 21:46:38 +0300

    homework 13 added one-to-one start ci

---
+++ tests/002user-controller-check-one-user.test.ts
@@ -0,0 +1,43 @@
+import {expect, test} from "@playwright/test";
+import {StatusCodes} from "http-status-codes";
+
+let userID: number;
+let baseURL: string = 'http://localhost:3000/users';
+
+test.beforeAll(async ({request}) => {
+    const response = await request.post(`${baseURL}`);
+    const body = await response.json();
+    userID = body.id
+});
+test.describe('Check for one User management  API', () => {
+
+    test('POST / - should add a new user', async ({request}) => {
+        const response = await request.post(`${baseURL}`);
+        const body = await response.json();
+        expect(response.status()).toBe(201)
+        expect.soft(response.status()).toBe(StatusCodes.CREATED)
+        expect.soft(body.id).toBeDefined()
+    });
+
+    test('GET /:id - should return a user by ID', async ({request}) => {
+        const response = await request.get(`${baseURL}` + "/" + userID);
+        const responseBody = await response.text()
+        console.log(responseBody)
+        expect(response.status()).toBe(StatusCodes.OK);
+    });
+
+    test('DELETE /:id - should delete a user by ID', async ({request}) => {
+        const response = await request.post(`${baseURL}` + "/" + userID);
+        const responseBody = await response.json();
+        console.log(responseBody)
+        expect.soft(response.status()).toBe(StatusCodes.OK)
+        expect.soft(responseBody[0].id).toBe(userID)
+    });
+
+    test('DELETE /:id - should return 404 if user not found', async ({request}) => {
+        const response = await request.post(`${baseURL}` + "/" + 111);
+        const responseBody = await response.json();
+        console.log(responseBody)
+        // expect.soft(response.status()).toBe(StatusCodes.NOT_FOUND)
+    });
+});
--- tests/003user-controller-check-a-lot-of.test.ts
+++ tests/003user-controller-check-a-lot-of.test.ts
@@ -1,16 +1,12 @@
 // tests/api.spec.ts
 import {test, expect} from '@playwright/test';
 import {ApiClient} from "../src/api-client";
+import {STATUS_CODES} from "node:http";
+import {StatusCodes} from "http-status-codes";
 
 let baseURL: string = 'http://localhost:3000/users';
-
 let userID: number;
 
-// test.beforeAll(async ({ request }) => {
-//     const response = await request.post(`${baseURL}`);
-//     const body = await response.json();
-//     userID = body.id
-// });
 
 test.beforeEach(async ({ request }) => {
     // get all users
@@ -46,17 +42,11 @@ test.beforeEach(async ({ request }) => {
     expect(responseBodyEmpty).toBe('[]');
 })
 
-test.describe('User management API', () => {
-    test('GET / - should return empty when no users', async ({request}) => {
-        const response = await request.get(`${baseURL}`);
-        expect(response.status()).toBe(200);
-        const responseBody = await response.text()
-        expect(response.status()).toBe('[]');
-    });
+test.describe('Check for five Users management API', () => {
 
     test('POST create n users', async ({request}) => {
         const apiClient = await ApiClient.getInstance(request)
-        const usersCount = await apiClient.createUsers(5)
+        const usersCount = await apiClient.createUsers(3)
         const response = await request.get(`${baseURL}`);
         const responseBody = await response.json()
         let numberOfObject = responseBody.length
@@ -65,7 +55,7 @@ test.describe('User management API', () => {
 
     test('DELETE n users', async ({request}) => {
         const apiClient = await ApiClient.getInstance(request)
-        const usersCount = await apiClient.createUsers(5)
+        const usersCount = await apiClient.createUsers(3)
         let userIDs = [];
         const response = await request.get(`${baseURL}`);
         const responseBody = await response.json()
@@ -91,40 +81,5 @@ test.describe('User management API', () => {
         const expectResponseBody = await expectResponse.json()
 
         expect(expectResponseBody).toStrictEqual([])
-    });
-
-
-    test('GET /:id - should return a user by ID', async ({request}) => {
-        const response = await request.get(`${baseURL}` + "/" + userID);
-        const responseBody = await response.text()
-        console.log(responseBody)
-        expect(response.status()).toBe(200);
-    });
-
-    test('GET /:id - should return 404 if user not found', async ({request}) => {
-
-    });
-
-    test('POST / - should add a new user', async ({request}) => {
-        const response = await request.post(`${baseURL}`);
-        const body = await response.json();
-        expect(response.status()).toBe(201)
-        expect.soft(response.status()).toBe(201)
-        expect.soft(body.id).toBeDefined()
-    });
-
-    test('DELETE /:id - should delete a user by ID', async ({request}) => {
-        const response = await request.post(`${baseURL}` + "/" + userID);
-        const responseBody = await response.json();
-        console.log(responseBody)
-        expect.soft(response.status()).toBe(200)
-        expect.soft(responseBody[0].id).toBe(userID)
-    });
-
-    test('DELETE /:id - should return 404 if user not found', async ({request}) => {
-        const response = await request.post(`${baseURL}` + "/" + 111);
-        const responseBody = await response.json();
-        console.log(responseBody)
-        expect.soft(response.status()).toBe(404)
     });
 });
